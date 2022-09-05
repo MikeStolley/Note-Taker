4526e3daafb8e64e87b8aa3cda4a94a666877ead
Mike Stolley <stolleywilliam@yahoo.com>
2022-09-05 12:33:52 -0400

    added server.js and other files for structure, added listener, parsing, and routes required into server.js

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Use routes located in routes folder
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// pull from folder given to us
+app.use(express.static('public'));
+
+// parse, JSON requirement
+app.use(express.json());
+app.use(urlencoded({ encoded: true }));
+
+app.listen(PORT, () => {
+    console.log(`API server on port ${PORT}!`)
+});
