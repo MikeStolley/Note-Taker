bbe9fcab439525d2f12fcd0220b945c4aa79e727
Mike Stolley <stolleywilliam@yahoo.com>
2022-09-05 13:40:49 -0400

    added functions to read write, and (hopefully) delete notes

---
+++ Develop/db/storeFile.js
@@ -0,0 +1,46 @@
+const util = require('util');
+const fs = require('fs');
+const uuid = require('uuid');
+const { throws } = require('assert');
+
+const readFileSync = util.promisify(fs.readFile);
+const writeFileSync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileSync("db/db.json", 'utf-8')
+    }
+
+    write() {
+        return writeFileSync("db/db.json", JSON.stringify(note));
+    }
+
+    addNote(note) {
+        const { title, text } = note
+        if(!title || !text) {
+            throw new Error("Must enter title and text")
+        }
+
+        const newNotes = { title, text, id: uuid() }
+
+        return this.getNote()
+            .then (notes => [...notes, newNotes])
+            .then (refreshNote => this.write(refreshNote))
+            .then(() => this.newNotes)
+    }
+
+    getNote() {
+        return this.read()
+            .then(notes => {
+                return JSON.parse(notes) || [];
+            })
+    }
+
+    deleteNote(id) {
+        return this.getNote()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(keepNotes => this.write(keepNotes))
+    }
+}
+
+module.exports = new Store();
